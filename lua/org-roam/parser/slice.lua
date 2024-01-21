300468240b34b2f59d115bd261e956519defd222
Chip Senkbeil <chip@senkbeil.org>
2024-01-21 12:28:20 -0600

    Unfinished refactoring to use slice across parsed data

--- lua/org-roam/parser/slice.lua
+++ lua/org-roam/parser/slice.lua
@@ -1,3 +1,6 @@
+local Range = require("org-roam.parser.range")
+local Ref = require("org-roam.parser.ref")
+
 -------------------------------------------------------------------------------
 -- SLICE.LUA
 --
@@ -5,23 +8,158 @@
 -------------------------------------------------------------------------------
 
 ---@class org-roam.parser.Slice
----@field text string #text represented by the range (not full text)
----@field range org-roam.parser.Range
+---@field private __full org-roam.parser.Ref<string> #reference to full text
+---@field private __cache string #cache to the text represented by this slice
+---@field private __range org-roam.parser.Range
 local M = {}
 M.__index = M
 
----Creates a new slice.
----@param text string
+---Creates a new slice for the given text using the provided range as the subset.
+---@param text org-roam.parser.Ref<string>
 ---@param range org-roam.parser.Range
 ---@return org-roam.parser.Slice
 function M:new(text, range)
     local instance = {}
     setmetatable(instance, M)
 
-    instance.text = text
-    instance.range = range
+    instance.__full = text
+    instance.__range = range
 
     return instance
+end
+
+---Creates a new slice for the given range using the same contents.
+---@return org-roam.parser.Slice
+function M:slice(range)
+    return M:new(self.__full, range)
+end
+
+---Creates a new slice encompassing an entire string reference.
+---@param ref org-roam.parser.Ref<string>
+---@return org-roam.parser.Slice
+function M:from_ref(ref)
+    -- Calculate the range of the string by checking how
+    -- many lines it has and how many characters on the
+    -- last line to serve as the ending column.
+    --
+    -- Keep in mind that our range is INCLUSIVE for the end!
+    local text_len = string.len(ref.value)
+
+    ---@type integer|nil
+    local newline_offset
+    local line_cnt = 1
+
+    ---@type integer|nil
+    local i = 0
+    while true do
+        i = string.find(ref.value, "\n", i + 1)
+        if i == nil then break end
+
+        -- Adjust offset to be zero-indexed
+        newline_offset = i - 1
+
+        -- Increment total lines since we found another newline
+        line_cnt = line_cnt + 1
+    end
+
+
+    local range = Range:new({
+        row = 0,
+        column = 0,
+        offset = 0,
+    }, {
+        -- Final row (line) in text, zero-indexed
+        row = line_cnt - 1,
+        -- Final column in last line of text, zero-indexed
+        column = newline_offset and (text_len - newline_offset - 1) or (text_len - 1),
+        -- Final byte offset of text, zero-indexed
+        offset = text_len - 1,
+    })
+
+    return M:new(ref, range)
+end
+
+---Creates a new slice encompassing an entire string.
+---@param text string
+---@return org-roam.parser.Slice
+function M:from_string(text)
+    local ref = Ref:new(text)
+    return M:from_ref(ref)
+end
+
+---Returns copy of text represented by the slice.
+---Note that this is cached after first calculation, unless an option is provided to force refresh.
+---@param opts? {refresh?:boolean}
+---@return string
+function M:text(opts)
+    opts = opts or {}
+
+    -- Calculate the actual text if not cached or forcing refresh
+    if not self.__cache or opts.refresh then
+        -- NOTE: Range values are zero-indexed, so we need to transform them
+        --       into Lua's one-indexed version!
+        self.__cache = string.sub(
+            self.__full.value,
+            self.__range.start.offset + 1,
+            self.__range.end_.offset + 1
+        )
+    end
+
+    return self.__cache
+end
+
+---Returns a copy of the range represented by the slice.
+---@return org-roam.parser.Range
+function M:range()
+    return vim.deepcopy(self.__range)
+end
+
+---Returns a copy of the start position of the slice.
+---@return org-roam.parser.Position
+function M:start_position()
+    return vim.deepcopy(self.__range.start)
+end
+
+---Returns a copy of the end position of the slice.
+---@return org-roam.parser.Position
+function M:end_position()
+    return vim.deepcopy(self.__range.end_)
+end
+
+---Returns the start row (zero-indexed) of the slice.
+---@return integer
+function M:start_row()
+    return self.__range.start.row
+end
+
+---Returns the start column (zero-indexed) of the slice.
+---@return integer
+function M:start_column()
+    return self.__range.start.column
+end
+
+---Returns the start byte offset (zero-indexed) of the slice.
+---@return integer
+function M:start_byte_offset()
+    return self.__range.start.offset
+end
+
+---Returns the end row (zero-indexed) of the slice.
+---@return integer
+function M:end_row()
+    return self.__range.end_.row
+end
+
+---Returns the end column (zero-indexed) of the slice.
+---@return integer
+function M:end_column()
+    return self.__range.end_.column
+end
+
+---Returns the end byte offset (zero-indexed) of the slice.
+---@return integer
+function M:end_byte_offset()
+    return self.__range.end_.offset
 end
 
 return M
