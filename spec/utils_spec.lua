4130a1403d4affe8e96e93fda4caa50f591ca860
Chip Senkbeil <chip@senkbeil.org>
2024-01-23 22:55:52 -0800

    Add async write file to utilities

--- spec/utils_spec.lua
+++ spec/utils_spec.lua
@@ -50,4 +50,50 @@ describe("utils", function()
             assert.equals("test", contents)
         end)
     end)
+
+    describe("async_write_file", function()
+        it("should create a file if none exists", function()
+            local error
+            local is_done = false
+
+            local path = vim.fn.tempname()
+            utils.async_write_file(path, "hello world", function(err)
+                error = err
+                is_done = true
+            end)
+
+            vim.wait(10000, function() return is_done end)
+
+            local f = assert(io.open(path, "r"))
+            local contents = f:read("*a")
+            f:close()
+
+            assert.is_nil(error)
+            assert.equals("hello world", contents)
+        end)
+
+        it("should overwrite a file if it exists", function()
+            local error
+            local is_done = false
+
+            local path = vim.fn.tempname()
+            local f = assert(io.open(path, "w"))
+            f:write("test")
+            f:close()
+
+            utils.async_write_file(path, "hello world", function(err)
+                error = err
+                is_done = true
+            end)
+
+            vim.wait(10000, function() return is_done end)
+
+            f = assert(io.open(path, "r"))
+            local contents = f:read("*a")
+            f:close()
+
+            assert.is_nil(error)
+            assert.equals("hello world", contents)
+        end)
+    end)
 end)
