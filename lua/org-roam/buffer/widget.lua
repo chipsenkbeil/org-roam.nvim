4a97b21a2929682f5731c079d211a06adb916584
Chip Senkbeil <chip@senkbeil.org>
2024-03-04 22:39:03 -0600

    Refactor some ui stuff

--- lua/org-roam/buffer/widget.lua
+++ lua/org-roam/buffer/widget.lua
@@ -5,24 +5,9 @@
 -------------------------------------------------------------------------------
 
 ---@class org-roam.buffer.Widget
-local M = {}
-M.__index = M
-
----Creates a new widget.
----@return org-roam.buffer.Widget
-function M:new()
-    local instance = {}
-    setmetatable(instance, M)
-    return instance
-end
+---@field apply fun(opts:org-roam.buffer.widget.ApplyOpts)
 
 ---@class org-roam.buffer.widget.ApplyOpts
 ---@field buffer integer
 ---@field database org-roam.core.database.Database
 ---@field node org-roam.core.database.Node
-
----@param opts org-roam.buffer.widget.ApplyOpts
----@diagnostic disable-next-line:unused-local
-function M:apply(opts) end
-
-return M
