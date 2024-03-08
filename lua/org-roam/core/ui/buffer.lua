e3079b92f47d0592f04089899f38381c0ab078a8
Chip Senkbeil <chip@senkbeil.org>
2024-03-07 20:31:57 -0600

    More refactoring work

--- lua/org-roam/core/ui/buffer.lua
+++ lua/org-roam/core/ui/buffer.lua
@@ -4,34 +4,63 @@
 -- Buffer interface for org-roam.
 -------------------------------------------------------------------------------
 
-local buffer = require("org-roam.core.buffer")
 local notify = require("org-roam.core.ui.notify")
 local utils = require("org-roam.core.utils")
 
+local EVENTS = {
+    ---When rendering is about to start.
+    PRE_RENDER = "pre_render",
+
+    ---When rendering has just finished.
+    POST_RENDER = "post_render",
+}
+
+---@enum org-roam.core.ui.buffer.State
+local STATE = {
+    ---When the buffer is not scheduled or rendering.
+    IDLE = "idle",
+
+    ---When the buffer is scheduled for rendering.
+    SCHEDULED = "scheduled",
+
+    ---When the buffer is in the midst of rendering.
+    RENDERING = "rendering",
+}
+
 ---@class org-roam.core.ui.Buffer
 ---@field private __bufnr integer
----@field private __db org-roam.core.database.Database
 ---@field private __emitter org-roam.core.utils.Emitter
----@field private __node org-roam.core.database.Id|nil
----@field private __rendering boolean
----@field private __scheduled boolean
+---@field private __state org-roam.core.ui.buffer.State
 ---@field private __widgets org-roam.core.ui.Widget[]
 local M = {}
 M.__index = M
 
----@enum org-roam.core.ui.buffer.Events
-local EVENTS = {
-    BUFFER_CHANGED = "buffer:changed",
-}
-M.EVENTS = EVENTS
+---Makes a new buffer configured for orgmode.
+---@param opts? {filetype?:string, name?:string, listed?:boolean, scratch?:boolean}
+---@return integer
+local function make_buffer(opts)
+    opts = opts or {}
 
----@class org-roam.core.ui.buffer.NewOpts
----@field db org-roam.core.database.Database
----@field buffer? integer #if specified, will replace this buffer's contents
----@field node? org-roam.core.database.Id #if specified, will render using this id, otherwise renders node under cursor
+    local bufnr = vim.api.nvim_create_buf(
+        type(opts.listed) == "boolean" and opts.listed or false,
+        type(opts.scratch) == "boolean" and opts.scratch or true
+    )
+    assert(bufnr ~= 0, "failed to create buffer")
+
+    -- Set name to something random (unless specified)
+    vim.api.nvim_buf_set_name(
+        bufnr,
+        opts.name or ("org-roam-" .. utils.random.uuid_v4())
+    )
+
+    -- Set the filetype to org (unless specified) because we're all in, baby!
+    vim.api.nvim_buf_set_option(bufnr, "filetype", opts.filetype or "org")
+
+    return bufnr
+end
 
 ---Creates a new org-roam buffer.
----@param opts org-roam.core.ui.buffer.NewOpts
+---@param opts? {filetype?:string, name?:string, listed?:boolean, scratch?:boolean}
 ---@return org-roam.core.ui.Buffer
 function M:new(opts)
     opts = opts or {}
@@ -39,112 +68,101 @@ function M:new(opts)
     local instance = {}
     setmetatable(instance, M)
 
-    local emitter        = utils.emitter:new()
-    instance.__bufnr     = opts.buffer
-    instance.__db        = opts.db
-    instance.__emitter   = emitter
-    instance.__node      = opts.node
-    instance.__rendering = false
-    instance.__scheduled = false
-    instance.__widgets   = {}
-
-    -- Register event when buffer receives a changed event
-    -- to force it to re-render.
-    emitter:on(EVENTS.BUFFER_CHANGED, function()
-        instance:__render()
-    end)
+    local emitter      = utils.emitter:new()
+    instance.__bufnr   = make_buffer(opts)
+    instance.__emitter = emitter
+    instance.__state   = STATE.IDLE
+    instance.__widgets = {}
 
     return instance
 end
 
----Configure a widget to be used with this buffer.
+---Adds a widget to be used with this buffer.
 ---@param widget org-roam.core.ui.Widget
 ---@return org-roam.core.ui.Buffer
-function M:widget(widget)
+function M:add_widget(widget)
     table.insert(self.__widgets, widget)
     return self
 end
 
----@param opts? {window?:integer|(fun(bufnr:integer):integer)}
-function M:open(opts)
-    opts = opts or {}
+---Returns handle to underlying buffer.
+---@return integer
+function M:bufnr()
+    return self.__bufnr
+end
 
-    self:__configure_buffer()
-
-    -- Begin refreshing the buffer before showing it
-    self:__render()
-
-    -- Show it in the user-specified window, or the current window
-    local window = opts.window or 0
-    if type(window) == "function" then
-        window = window(self.__bufnr)
-    end
-    if vim.api.nvim_win_get_buf(window) ~= self.__bufnr then
-        vim.api.nvim_win_set_buf(window, self.__bufnr)
-    end
+---Appends the provided lines to the end of the buffer.
+---@param lines string[]
+function M:append_lines(lines)
+    vim.api.nvim_buf_set_lines(self.__bufnr, -1, -1, true, lines)
 end
 
 ---@private
----@param cb? fun()
-function M:__render(cb)
-    local function report_done()
-        if cb then
-            vim.schedule(cb)
-        end
+---Clears the buffer's contents.
+function M:__clear()
+    vim.api.nvim_buf_set_lines(self.__bufnr, 0, -1, true, {})
+end
 
-        self.__rendering = false
-    end
+---Invokes `cb` right before render has started.
+---@param cb fun()
+function M:on_pre_render(cb)
+    self.__emitter:on(EVENTS.PRE_RENDER, cb)
+end
 
-    -- If we were already rendering, schedule for later and exit
-    if self.__rendering then
-        -- We only schedule a render once as subsequent requests
-        -- all get merged together for the next successful render
-        if not self.__scheduled then
-            self.__scheduled = true
-            vim.schedule(function()
-                self.__scheduled = false
-                self:__render(cb)
-            end)
-        end
+---Invokes `cb` once render has finished.
+---@param cb fun()
+function M:on_post_render(cb)
+    self.__emitter:on(EVENTS.POST_RENDER, cb)
+end
 
+---Returns the current state of the buffer.
+---@return org-roam.core.ui.buffer.State
+function M:state()
+    return self.__state
+end
+
+---@return boolean
+function M:is_idle()
+    return self.__state == STATE.IDLE
+end
+
+---@return boolean
+function M:is_scheduled()
+    return self.__state == STATE.SCHEDULED
+end
+
+---@return boolean
+function M:is_rendering()
+    return self.__state == STATE.RENDERING
+end
+
+---Clear and redraw the buffer using the current widgets.
+---
+---If `delay` specified, will wait N milliseconds before scheduling rendering.
+---If `sync` specified, will render directly instead of scheduling rendering.
+---
+---@param opts? {delay?:integer, sync?:boolean}
+function M:render(opts)
+    opts = opts or {}
+
+    -- Prevent accidental repeated render calls
+    if self.__state ~= STATE.IDLE then
         return
     end
 
-    -- Mark ourselves as now rendering to avoid spam
-    self.__rendering = true
+    local function do_render()
+        -- Report that we are about to render
+        self.__emitter:emit(EVENTS.PRE_RENDER)
 
-    local bufnr = self.__bufnr
+        -- Mark as in the rendering state
+        self.__state = STATE.RENDERING
 
-    -- Load the node to render
-    self:__get_node(function(node)
-        -- Set buffer to contain the buffer's name first
+        -- Clear the buffer of its content
         self:__clear()
-        self:__append_lines({
-            string.format("# %s", vim.api.nvim_buf_get_name(bufnr)),
-        })
 
-        -- If we have no node available, then we're done
-        if not node then
-            report_done()
-            return
-        end
-
-        -- Append the name of the node
-        self:__append_lines({ "", string.format("# %s", node.title) })
-
-        ---@param lines string[]
-        local function append(lines)
-            self:__append_lines(lines)
-        end
-
-        -- For each widget, generate content
+        -- Redraw content using the provided widgets
         for _, widget in ipairs(self.__widgets) do
-            local ok, err = widget:render({
-                append  = append,
-                db      = self.__db,
-                emitter = self.__emitter,
-                node    = node,
-            })
+            local ok, err = widget:render(self)
 
             if not ok and not err then
                 err = "unexpected error during rendering of widget"
@@ -155,51 +173,23 @@ function M:__render(cb)
             end
         end
 
-        -- Rendering has finished!
-        report_done()
-    end)
-end
+        -- Reset to idle state as we're done
+        self.__state = STATE.IDLE
 
----@private
----@return integer bufnr
-function M:__configure_buffer()
-    -- Set or create a buffer
-    local bufnr = self.__bufnr or vim.api.nvim_create_buf(false, true)
-    assert(bufnr ~= 0, "failed to create buffer")
-
-    -- Set name to something random
-    vim.api.nvim_buf_set_name(bufnr, "org-roam-" .. utils.random.uuid_v4())
-
-    -- Set the filetype to org because we're all in, baby!
-    vim.api.nvim_buf_set_option(bufnr, "filetype", "org")
-
-    -- Update and return the buffer handle
-    self.__bufnr = bufnr
-    return bufnr
-end
-
----@private
----@param cb fun(node:org-roam.core.database.Node|nil)
-function M:__get_node(cb)
-    local db = self.__db
-    if self.__node then
-        cb(db:get(self.__node))
-    else
-        buffer.node_under_cursor(function(id)
-            cb(id and db:get(id))
-        end)
+        -- Report that we have rendered
+        self.__emitter:emit(EVENTS.POST_RENDER)
     end
-end
 
----@private
----@param lines string[]
-function M:__append_lines(lines)
-    vim.api.nvim_buf_set_lines(self.__bufnr, -1, -1, true, lines)
-end
+    -- Mark as scheduled to render
+    self.__state = STATE.SCHEDULED
 
----@private
-function M:__clear()
-    vim.api.nvim_buf_set_lines(self.__bufnr, 0, -1, true, {})
+    if opts.sync then
+        do_render()
+    elseif opts.delay then
+        vim.defer_fn(do_render, opts.delay)
+    else
+        vim.schedule(do_render)
+    end
 end
 
 return M
--- lua/org-roam/core/ui/widget.lua
+++ lua/org-roam/core/ui/widget.lua
@@ -5,43 +5,26 @@
 -------------------------------------------------------------------------------
 
 ---@class org-roam.core.ui.Widget
----@field private __render fun(opts:org-roam.core.ui.widget.RenderOpts)
----@field private __rendering boolean
+---@field private __render fun(buffer:org-roam.core.ui.Buffer)
 local M = {}
 M.__index = M
 
 ---Creates a new org-roam ui widget.
----@param f fun(opts:org-roam.core.ui.widget.RenderOpts)
+---@param render fun(buffer:org-roam.core.ui.Buffer)
 ---@return org-roam.core.ui.Widget
-function M:new(f)
+function M:new(render)
     local instance = {}
     setmetatable(instance, M)
 
-    instance.__render = f
-    instance.__rendering = false
+    instance.__render = render
 
     return instance
 end
 
----@class org-roam.core.ui.widget.RenderOpts
----@field append fun(lines:string[])
----@field db org-roam.core.database.Database
----@field emitter org-roam.core.utils.Emitter
----@field node org-roam.core.database.Node
-
----@param opts org-roam.core.ui.widget.RenderOpts
+---@param buffer org-roam.core.ui.Buffer
 ---@return boolean ok, string|nil err
-function M:render(opts)
-    if self.__rendering then
-        return true
-    end
-
-    self.__rendering = true
-
-    local ok, err = pcall(self.__render, opts)
-
-    self.__rendering = false
-
+function M:render(buffer)
+    local ok, err = pcall(self.__render, buffer)
     return ok, err and vim.inspect(err)
 end
 
