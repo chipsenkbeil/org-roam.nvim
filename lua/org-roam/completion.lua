560bc8daffc63f8b19416f509362b297ca8bfb3e
Chip Senkbeil <chip@senkbeil.org>
2024-03-16 00:39:16 -0500

    Minor cleanup

--- lua/org-roam/completion.lua
+++ lua/org-roam/completion.lua
@@ -4,51 +4,10 @@
 -- Contains functionality tied to org-roam completion.
 -------------------------------------------------------------------------------
 
-local async = require("org-roam.core.utils.async")
 local database = require("org-roam.database")
 local utils = require("org-roam.utils")
 
 local M = {}
-
----Function used to interface with the complete function.
----
----This will perform completion by search for nodes that start with the
----current `arg_lead` based on their id, title, or alias.
----
----@param arg_lead string #leading portion of the argument being completed
----@param cmd_line string #entire command line
----@param cursor_pos integer #cursor position in cmdline (byte index)
----@return string #candidates separated by newline
-function M.CompleteNode(arg_lead, cmd_line, cursor_pos)
-    local db = database()
-
-    print("complete")
-    return table.concat({
-        string.format("%s-1", arg_lead),
-        string.format("%s-2", arg_lead),
-        string.format("%s-3", arg_lead),
-    }, "\n")
-    --[[
-    -- Hack: Monkey-patch buffer keymap upon the first completion (<Tab>)
-    -- TODO: This is taken from dap.lua, which uses nvim-cmp plugin.
-    --       Since this needs to be a standalone (beyond orgmode), we
-    --       need to figure out how to do this ourselves.
-    local bufnr = vim.api.nvim_get_current_buf()
-    vim.api.nvim_buf_set_option(bufnr, "filetype", "dapui_eval_input")
-    cmp.setup.buffer({ enabled = true })
-    vim.keymap.set("i", "<Tab>", function()
-        if cmp.visible() then
-            cmp.select_next_item()
-        else
-            cmp.complete()
-        end
-    end, { buffer = bufnr })
-    vim.keymap.set("i", "<S-Tab>", function()
-        if cmp.visible() then cmp.select_prev_item() end
-    end, { buffer = bufnr })
-
-    return "" ]]
-end
 
 ---Opens a dialog to select a node based on the expression under the cursor
 ---and replace the expression with a link to the selected node. If there is
