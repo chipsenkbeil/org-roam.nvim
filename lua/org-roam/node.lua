2ac7848bb1f7613e6a665b56af7730d8d131265f
Chip Senkbeil <chip@senkbeil.org>
2024-03-24 15:36:38 -0500

    Minor tweak

--- lua/org-roam/node.lua
+++ lua/org-roam/node.lua
@@ -156,6 +156,7 @@ function M.capture(opts, cb)
         files = plugin.files,
         templates = templates,
         on_close = function(_, opts)
+            local filename = opts.destination_file.filename
             local id = opts.source_file:get_property("ID")
 
             -- If we don't find a file-level node, look for headline nodes
@@ -166,7 +167,8 @@ function M.capture(opts, cb)
                 end
             end
 
-            vim.schedule(function() cb(id) end)
+            -- TODO: Load new file and populate database
+            cb(id)
         end,
     })
 
