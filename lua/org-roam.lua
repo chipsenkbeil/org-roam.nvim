da9a3dad6484649ccf0d4b0aa65aca1e6224d16d
Chip Senkbeil <chip@senkbeil.org>
2024-04-14 23:48:39 -0500

    Add untested metatable to top-level plugin that will throw errors when accessing key fields that have not yet been initialized

--- lua/org-roam.lua
+++ lua/org-roam.lua
@@ -15,7 +15,22 @@ end
 ---@field db org-roam.Database
 ---@field evt org-roam.events.Emitter
 ---@field ext org-roam.Extensions
-local M = {}
+local M = setmetatable({}, {
+    __index = function(tbl, key)
+        local value = rawget(tbl, key)
+        if type(value) == "nil" then
+            local init_list = { "api", "config", "db", "evt", "ext" }
+            if vim.tbl_contains(init_list, key) then
+                error(table.concat({
+                    "Roam plugin not yet initialized!",
+                    "Cannot access roam." .. key .. ".",
+                    "Invoke roam:setup(...) first.",
+                }, " "))
+            end
+        end
+        return value
+    end,
+})
 
 ---Initializes the plugin.
 ---
