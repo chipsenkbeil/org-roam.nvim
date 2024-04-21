c5f8332c3032751df7968f7b066bf202fdd00b22
Chip Senkbeil <chip@senkbeil.org>
2024-04-20 22:20:02 -0500

    Finish node buffer tests

--- spec/ui_node_buffer_spec.lua
+++ spec/ui_node_buffer_spec.lua
@@ -153,10 +153,123 @@ describe("org-roam.ui.node-buffer", function()
         }, utils.read_buffer(buf))
     end)
 
-    it("should display node buffer for a fixed node specified", function()
+    it("should be able to navigate to the link from the node buffer", function()
+        roam.db:load():wait()
+
+        -- Load up a test file so we have a node under cursor
+        vim.cmd.edit(test_path_two)
+
+        -- Get the window that contains the node
         local node_win = vim.api.nvim_get_current_win()
 
+        ---@type integer|nil
+        local win = roam.ui.toggle_node_buffer({ focus = true }):wait()
+        assert(win and win ~= 0, "failed to create node buffer window")
+
+        -- Jump to the line containing the origin and trigger navigation
+        utils.jump_to_line(function(_, lines)
+            for i, line in ipairs(lines) do
+                if string.find(line, "one @ 7,0") then
+                    return i
+                end
+            end
+        end)
+
+        -- Trigger our buffer-local mapping, waiting a bit for the node to load
+        utils.trigger_mapping("n", "<CR>", { buf = 0, wait = 100 })
+
+        -- Load the non-node buffer that is visible
+        local buf = vim.api.nvim_win_get_buf(node_win)
+        assert(buf ~= 0, "failed to retrieve node buffer handle")
+
+        assert.are.same({
+            ":PROPERTIES:",
+            ":ID: 1",
+            ":ROAM_ALIASES: one",
+            ":END:",
+            "#+FILETAGS: :one:",
+            "",
+            "[[id:2]]",
+        }, utils.read_buffer(buf))
+    end)
+
+    it("should be able to expand a link to display a preview within the node buffer", function()
         roam.db:load():wait()
+
+        -- Load up a test file so we have a node under cursor
+        vim.cmd.edit(test_path_two)
+
+        ---@type integer|nil
+        local win = roam.ui.toggle_node_buffer({ focus = true }):wait()
+        assert(win and win ~= 0, "failed to create node buffer window")
+
+        local buf = vim.api.nvim_win_get_buf(win)
+        assert(buf ~= 0, "failed to retrieve node buffer handle")
+
+        -- Jump to the line containing the origin and trigger navigation
+        utils.jump_to_line(function(_, lines)
+            for i, line in ipairs(lines) do
+                if string.find(line, "one @ 7,0") then
+                    return i
+                end
+            end
+        end)
+
+        -- Trigger our buffer-local mapping, waiting a bit for the preview
+        utils.trigger_mapping("n", "<Tab>", { buf = 0, wait = 100 })
+
+        assert.are.same({
+            "Press <Enter> to open a link in another window",
+            "Press <Tab> to expand/collapse a link",
+            "Press <S-Tab> to expand/collapse all links",
+            "Press <C-r> to refresh buffer",
+            "",
+            "Node: two",
+            "Origin: one",
+            "",
+            "Backlinks (1)",
+            "▼ one @ 7,0",
+            "[[id:2]]",
+        }, utils.read_buffer(buf))
+    end)
+
+    it("should be able to expand all links to display previews within the node buffer", function()
+        roam.db:load():wait()
+
+        -- Load up a test file so we have a node under cursor
+        vim.cmd.edit(test_path_two)
+
+        ---@type integer|nil
+        local win = roam.ui.toggle_node_buffer({ focus = true }):wait()
+        assert(win and win ~= 0, "failed to create node buffer window")
+
+        local buf = vim.api.nvim_win_get_buf(win)
+        assert(buf ~= 0, "failed to retrieve node buffer handle")
+
+        -- Trigger our buffer-local mapping, waiting a bit for the preview
+        utils.trigger_mapping("n", "<S-Tab>", { buf = 0, wait = 100 })
+
+        assert.are.same({
+            "Press <Enter> to open a link in another window",
+            "Press <Tab> to expand/collapse a link",
+            "Press <S-Tab> to expand/collapse all links",
+            "Press <C-r> to refresh buffer",
+            "",
+            "Node: two",
+            "Origin: one",
+            "",
+            "Backlinks (1)",
+            "▼ one @ 7,0",
+            "[[id:2]]",
+        }, utils.read_buffer(buf))
+    end)
+
+    it("should display node buffer for a fixed node specified", function()
+        roam.db:load():wait()
+
+        -- Load up multiple different files
+        local one_win, two_win = utils.edit_files(test_path_one, test_path_two)
+        vim.api.nvim_set_current_win(one_win)
 
         ---@type integer|nil
         local win = roam.ui.toggle_node_buffer({ fixed = "1" }):wait()
@@ -178,11 +291,10 @@ describe("org-roam.ui.node-buffer", function()
         }, utils.read_buffer(buf))
 
         -- Switch back to our node window and change it to move cursor
-        vim.api.nvim_set_current_win(node_win)
-        vim.cmd.edit(test_path_three)
+        vim.api.nvim_set_current_win(two_win)
         vim.wait(100)
 
-        -- Verify it is unchanged
+        -- Verify it is unchanged despite cursor moving
         assert.are.same({
             "Press <Enter> to open a link in another window",
             "Press <Tab> to expand/collapse a link",
