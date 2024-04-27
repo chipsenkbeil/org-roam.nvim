a3746cb8ff73872e869d85a8727268ac56b8ea39
Chip Senkbeil <chip@senkbeil.org>
2024-04-27 11:30:45 -0500

    Outline methods for dailies extension

--- lua/org-roam/extensions/dailies.lua
+++ lua/org-roam/extensions/dailies.lua
@@ -12,5 +12,46 @@ return function(roam)
     ---@class org-roam.extensions.Dailies
     local M = {}
 
+    function M.capture_today()
+    end
+
+    function M.goto_today()
+    end
+
+    function M.capture_tomorrow()
+    end
+
+    function M.goto_tomorrow()
+    end
+
+    function M.capture_yesterday()
+    end
+
+    function M.goto_yesterday()
+    end
+
+    function M.capture_date()
+    end
+
+    function M.goto_date()
+    end
+
+    function M.goto_next_date()
+    end
+
+    function M.goto_prev_date()
+    end
+
+    function M.find_directory()
+    end
+
+    ---@private
+    function M.__list_files()
+    end
+
+    ---@private
+    function M.__is_daily_file()
+    end
+
     return M
 end
